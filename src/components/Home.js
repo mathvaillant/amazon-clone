a9f05167bdca1c256308805aaff303b9dc492752
mathvaillant <matheusvaillant505@gmail.com>
2020-11-27 11:19:28 +0000

    Product js added

---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Home.scss';
+import Ad from '../container ad.png';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="home__container">
+        <img 
+          className='home__container__ad'
+          src={Ad} 
+          alt="logo.png"/>
+          
+
+          <div className="home_row">
+            {/* Product */}
+            {/* Product */}
+          </div>
+
+          <div className="home_row">
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+          </div>
+
+          <div className="home_row">
+            {/* Product */}
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGsQhTRYo5bm3oaD3KOIV5ZG5lKXnRAWY",
+  authDomain: "full-stack-clone.firebaseapp.com",
+  databaseURL: "https://full-stack-clone.firebaseio.com",
+  projectId: "full-stack-clone",
+  storageBucket: "full-stack-clone.appspot.com",
+  messagingSenderId: "434810238095",
+  appId: "1:434810238095:web:36abe8e2074f36397aacfa"
+};
